7a04bd81f54fe336c28609854eabcc2910fe8098
Tran-Nguyen-26 <trannguyeen2005@gmail.com>
2025-10-19 14:23:32 +0700

    add entity location

--- backend/src/main/java/com/examreg/examreg/models/Room.java
+++ backend/src/main/java/com/examreg/examreg/models/Room.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
@@ -17,9 +19,11 @@ public class Room {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String roomCode;
+  private String name;
 
-  private String location;
+  @ManyToOne
+  @JoinColumn(name = "location_id")
+  private Location location;
 
   @OneToMany(mappedBy = "room")
   private List<ExamSession> examSessions;
